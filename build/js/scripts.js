83eb0c7add49542e88ede6ce4f4d9f7a667f0b63
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-01-20 18:22:17 -0200

    working version

--- build/js/scripts.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$(".container.passage").children().eq(1).html('<img src="img/passage/1.jpg"/>');
-});
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+var passage_current_photo = 0;
+var passage_length = 7;
+
 $(document).ready(function() {
-	$(".container.passage").children().eq(1).html('<img src="img/passage/1.jpg"/>');
+	$(".wrapper").html('<img src="img/passage/1.jpg"/>');
 });
+
+$("#prev").click(function() {
+	event.preventDefault();
+	if(passage_current_photo > 1) {
+		replacePhotoByPrevious("passage");
+	}
+});
+
+$("#next").click(function() {
+	event.preventDefault();
+	if(passage_current_photo != passage_length) {
+		replacePhotoByNext("passage");
+	}
+})
+
+var replacePhotoByPrevious = function(photoEssay) {
+	passage_current_photo = passage_current_photo - 1;
+	$(".wrapper").empty().html('<img src="img/passage/' + passage_current_photo + '.jpg"/>');
+}
+
+var replacePhotoByNext = function(photoEssay) {
+	passage_current_photo = passage_current_photo + 1;
+	$(".wrapper").empty().html('<img src="img/passage/' + passage_current_photo + '.jpg"/>');
+}
