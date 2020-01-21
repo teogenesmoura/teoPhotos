837e2ac24c5b2332aa841870d7f0ce092b684cb6
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-01-21 19:13:03 -0200

    added animation on series title

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,53 @@ var cage_current_photo = 0;
 var cage_length = 15;
 
 $(document).ready(function() {
-	appendTitleToInitialScreen();
+	initialScreen();
 });
 
-
-$("#prev").click(function() {
-	event.preventDefault();
-	if(cage_current_photo > 1) {
-		replacePhotoByPrevious("golden-cage");
-	} else {
-		appendTitleToInitialScreen();
-	}
-});
-
-$("#next").click(function() {
-	event.preventDefault();
-	if(cage_current_photo != cage_length) {
-		replacePhotoByNext("golden-cage");
-	} else {
-		cage_current_photo = 0;
-		appendTitleToInitialScreen();
-	}
-})
-
-var appendTitleToInitialScreen = function() {
-	$(".wrapper").empty().html('<div class="series-title"> Golden Cage </div>');
+var initialScreen = function() {
+	$(".navigation").empty();
+	setPhotoEssayTitles();
 }
+
+var setPhotoEssayTitles = function() {
+	$(".wrapper").empty().html('<div class="series-title fade"> Golden Cage </div>');
+	setClickEventsOnTitles();
+}
+
+var setClickEventsOnTitles = function() {
+	$(".series-title").click(function() {
+		event.preventDefault();
+		replacePhotoByNext("golden-cage");
+		setNavigationMenu();
+	});	
+}
+
+var setNavigationMenu = function() {
+	$(".navigation").html('<a href="#" id="prev"> Previous </a> <a href="#" id="next"> Next </a>');
+	setNavigationEvents();
+}
+
+var setNavigationEvents = function() {
+	$("#next").click(function() {
+		event.preventDefault();
+		if(cage_current_photo != cage_length) {
+			replacePhotoByNext("golden-cage");
+		} else {
+			cage_current_photo = 0;
+			initialScreen();
+		}
+	});
+
+	$("#prev").click(function() {
+		event.preventDefault();
+		if(cage_current_photo > 1) {
+			replacePhotoByPrevious("golden-cage");
+		} else {
+			initialScreen();
+		}
+	});	
+}
+
 
 var replacePhotoByPrevious = function(photoEssay) {
 	cage_current_photo = cage_current_photo - 1;
